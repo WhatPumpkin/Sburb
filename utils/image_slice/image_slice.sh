87d878a1b1b194fa6aa477dfaf6b97e5cc62669b
Lit Kladov <me@seq37.dev>
2020-04-20 23:31:04 +0300

    Add first prototype
    CAUTION: Does not work yet! Doesn't even load for some reason, the
    console is empty. If someone could fix that, that would be awesome.

--- utils/image_slice/image_slice.sh
+++ utils/image_slice/image_slice.sh
@@ -14,13 +14,14 @@ do
 	do
 		for y in $(seq 0 $size $height)
 		do
-			#echo $x $y
+			echo $x $y
 			name="../output/$purename"_"$(($x/$size))_$(($y/$size)).png"
-			convert $file -crop "$size"x"$size"+"$x"+"$y" $name
-			colspace=$(identify -verbose $name | grep Colorspace)
-			if [ "$colspace" = "  Colorspace: Gray" ]; then
-				rm $name
-			fi
+			echo $name
+			convert $file -crop "$size"x"$size"+"$x"+"$y" +repage $name
+			# colspace=$(identify -verbose $name | grep Colorspace)
+			# if [ "$colspace" = "  Colorspace: Gray" ]; then
+			# 	rm $name
+			# fi
 		done
 	done
 done
